edaec1855ae9991d0fc8e3d28275395943716468
Rahaman002 <36rahaman@gmail.com>
2024-01-23 13:27:18 +0530

    chore: faqs added

--- src/api/aboutus/routes/aboutus.js
+++ src/api/aboutus/routes/aboutus.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about router
+ * aboutus router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::about.about');
+module.exports = createCoreRouter('api::aboutus.aboutus');
--- src/api/aboutus/services/aboutus.js
+++ src/api/aboutus/services/aboutus.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about service
+ * aboutus service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::about.about');
+module.exports = createCoreService('api::aboutus.aboutus');
