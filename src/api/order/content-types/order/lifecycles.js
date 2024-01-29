3c438c08293f3d985b7ccb786a08c4ec5c64b1da
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-01-29 14:59:50 +0530

    chore: updating lifecycle hook of orders

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -3,29 +3,50 @@ module.exports = {
     const { result } = event;
     console.log("strapi data", result);
 
+    // fetch(`http://localhost:1337/api/orders/${result}`)
+    const data = await strapi.db.query("api::order.order").findOne({
+      where: {
+        id: result.id,
+      },
+      populate: {
+        // @ts-ignore
+        product: true,
+      },
+    });
+
+    console.log(data);
+
     if (!result.status) return;
 
     try {
-      // const productsHtml = result.product.map(product => `
-      //     <p>Product Name: ${product.name}</p>
-      //     <p>Product Quantity: ${product.quantity}</p>
-      // `).join('');
+      const productsHtml = data.product
+        .map(
+          (product) => `
+          <p>Product Name: ${product.name}</p>
+          <p>Product Quantity: ${product.quantity}</p>
+      `
+        )
+        .join("");
 
       await strapi.plugins["email"].services.email.send({
-        to: "kaparapu.akhilnaidu@gmail.com",
+        to: [
+          "kaparapu.akhilnaidu@gmail.com",
+          "jgarrison@ezhadvisoryservices.com",
+          data.emailAddress,
+        ],
         from: "akhil@contentql.io",
-        cc: "jagadeeshm778@gmail.com",
+        cc: "",
         bcc: "",
         replyTo: "akhil@contentql.io",
-        subject: "You got a new order !",
-        html: `<p>Name: ${result.firstName}</p>
-                    <p>Phone number: ${result.phoneNumber}</p>
-                    <p>Email: ${result.emailAddress}</p>
-                    <p>Street Address: ${result.streetAddress}</p> 
-                    <p>Zip code:${result.zipCode}</p>
-                    <p>city :${result.city}</p>
-                    <p>country:${result.country}</p>
-                    <p>Products:</p>`,
+        subject: "You got a new order!",
+        html: `<p>Name: ${data.firstName}</p>
+                    <p>Phone number: ${data.phoneNumber}</p>
+                    <p>Email: ${data.emailAddress}</p>
+                    <p>Street Address: ${data.streetAddress}</p> 
+                    <p>Zip code:${data.zipCode}</p>
+                    <p>city :${data.city}</p>
+                    <p>country:${data.country}</p>
+                    <p>Products:${productsHtml}</p>`,
       });
     } catch (err) {
       console.log(err);
