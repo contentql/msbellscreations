44b0be5d476a29a9492da92ed9728586ae362677
ManikantaPotnuru9176 <manikantapotnuru9176@gmail.com>
2024-01-29 14:05:06 +0530

    chore: commented the code in lifecycle

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,32 +1,34 @@
 module.exports = {
-    async afterUpdate(event) {
-        const { result } = event;
-        console.log("strapi data", result);
+  async afterUpdate(event) {
+    const { result } = event;
+    console.log("strapi data", result);
 
-        try {
-            const productsHtml = result.product.map(product => `
-                <p>Product Name: ${product.name}</p>
-                <p>Product Quantity: ${product.quantity}</p>
-            `).join('');
+    if (!result.status) return;
 
-            await strapi.plugins['email'].services.email.send({
-                to: 'kaparapu.akhilnaidu@gmail.com',
-                from: 'akhil@contentql.io',
-                cc: 'jagadeeshm778@gmail.com',
-                bcc: '',
-                replyTo: 'akhil@contentql.io',
-                subject: 'You got a new order !',
-                html: `<p>Name: ${result.firstName}</p>
+    try {
+      // const productsHtml = result.product.map(product => `
+      //     <p>Product Name: ${product.name}</p>
+      //     <p>Product Quantity: ${product.quantity}</p>
+      // `).join('');
+
+      await strapi.plugins["email"].services.email.send({
+        to: "kaparapu.akhilnaidu@gmail.com",
+        from: "akhil@contentql.io",
+        cc: "jagadeeshm778@gmail.com",
+        bcc: "",
+        replyTo: "akhil@contentql.io",
+        subject: "You got a new order !",
+        html: `<p>Name: ${result.firstName}</p>
                     <p>Phone number: ${result.phoneNumber}</p>
                     <p>Email: ${result.emailAddress}</p>
                     <p>Street Address: ${result.streetAddress}</p> 
                     <p>Zip code:${result.zipCode}</p>
                     <p>city :${result.city}</p>
                     <p>country:${result.country}</p>
-                    <p>${productsHtml}</p>`
-            });
-        } catch (err) {
-            console.log(err);
-        }
+                    <p>Products:</p>`,
+      });
+    } catch (err) {
+      console.log(err);
     }
+  },
 };
